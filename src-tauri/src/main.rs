e9fa1983a14cb85aa035275fe4476bb2d9e40124
p32929 <p32929@yahoo.com>
2022-09-06 18:55:35 +0600

    added single instance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,9 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+        }))
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
